1013eba047da3529ce73c35cb179775b08f66fdc
Aaron Beaudoin <aaron.beaudoin@aryse.com>
2022-08-29 15:08:29 -0500

    List headers in handlers 2

--- .vercel/output/functions/_default.func/index.js
+++ .vercel/output/functions/_default.func/index.js
@@ -2,7 +2,7 @@ export default async function handler(request) {
   return new Response([
     "Handler: _default",
     `URL: ${request.url}`,
-    `Rewrite: ${request.headers.get("x-middleware-url")}`,
-    `Headers: ${Array.from(request.headers.keys()).join(", ")}`
+    `Headers: ${Array.from(request.headers.keys()).join(", ")}`,
+    `Rewrite: ${request.headers.get("x-middleware-url")}`
   ].join("\n"));
 }
