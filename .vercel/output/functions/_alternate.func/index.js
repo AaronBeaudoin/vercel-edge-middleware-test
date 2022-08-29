c9b5be8ac47a74d2b9c6205f4c6d0f7a34ca12ec
Aaron Beaudoin <aaron.beaudoin@aryse.com>
2022-08-29 14:48:41 -0500

    Add location header to alt handler

--- .vercel/output/functions/_alternate.func/index.js
+++ .vercel/output/functions/_alternate.func/index.js
@@ -1,3 +1,3 @@
 export default async function handler(request) {
-  return new Response(`Hello from alternate handler edge function via ${request.url}`);
+  return new Response(`Hello from alternate handler edge function via ${request.url}\n at location ${request.headers.get("Location")}`);
 }
